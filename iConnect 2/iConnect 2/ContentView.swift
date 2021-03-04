4bd7c1523db47f5bd05883f1cd30c341746f9bda
Harry Dinh <thongdinh2004@icloud.com>
2021-03-04 15:38:29 -0500

    Initial commit

---
+++ iConnect 2/iConnect 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iConnect 2
+//
+//  Created by Harry Dinh on 2021-03-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iConnect 2/iConnect 2/iConnect_2App.swift
@@ -0,0 +1,17 @@
+//
+//  iConnect_2App.swift
+//  iConnect 2
+//
+//  Created by Harry Dinh on 2021-03-04.
+//
+
+import SwiftUI
+
+@main
+struct iConnect_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
