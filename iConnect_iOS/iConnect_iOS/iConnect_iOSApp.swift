446ec3636fbf9fd0e7ad51eee3f6bc63090cc13f
Harry Dinh <thongdinh2004@icloud.com>
2021-03-04 19:23:30 -0500

    Created iConnect_iOS

---
+++ iConnect_iOS/iConnect_iOS/iConnect_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConnect_iOSApp.swift
+//  iConnect_iOS
+//
+//  Created by Harry Dinh on 2021-03-04.
+//
+
+import SwiftUI
+
+@main
+struct iConnect_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
