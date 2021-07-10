fc53e53bf65ce247b9d4aef940374dbefbba8fd6
Harry Dinh <thongdinh2004@icloud.com>
2021-07-09 22:16:45 -0400

    Created iConDiscovery project

--- iConDiscovery/iConDiscovery/ContentView.swift
+++ iConDiscovery/iConDiscovery/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var urlBar = ""
+    @State private var returnPressed = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if !returnPressed {
+                Text("Nothing here...")
+            }
+            else {
+                WebView(pageURL: urlBar)
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .automatic) {
+                TextField("", text: $urlBar, onCommit: {
+                    returnPressed = true
+                })
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
--- iConDiscovery/iConDiscovery/WebView.swift
+++ iConDiscovery/iConDiscovery/WebView.swift
@@ -7,3 +7,39 @@
 
 import SwiftUI
 import WebKit
+
+class Observable: ObservableObject {
+    @Published var observation: NSKeyValueObservation?
+    @Published var loggedIn = false
+}
+
+
+struct WebView: NSViewRepresentable {
+    var pageURL: String
+    @ObservedObject var observe = Observable()
+    
+    func makeNSView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateNSView(_ uiView: WKWebView, context: Context) {
+        observe.observation = uiView.observe(\WKWebView.title, options: .new) { view, change in
+            if let title = view.title {
+                observe.loggedIn = true
+                print("Page loaded: \(title)")
+            }
+        }
+        
+        uiView.load(pageURL)
+    }
+}
+
+
+extension WKWebView {
+    func load(_ urlString: String) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            load(request)
+        }
+    }
+}
