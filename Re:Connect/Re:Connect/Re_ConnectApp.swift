febe09959c0726033078f41bb9296847e3af7e6a
Harry Dinh <thongdinh2004@icloud.com>
2021-07-17 13:37:54 -0400

    Created project Re:Connect, the replacement for iConnect

---
+++ Re:Connect/Re:Connect/Re_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Re_ConnectApp.swift
+//  Re:Connect
+//
+//  Created by Harry Dinh on 2021-07-17.
+//
+
+import SwiftUI
+
+@main
+struct Re_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
