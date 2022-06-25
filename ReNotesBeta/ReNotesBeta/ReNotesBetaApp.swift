77677245fa1064d9c91cac562d31b26eb271e844
Harry Dinh <thongdinh2004@icloud.com>
2022-06-25 13:24:18 -0400

    Deleted project ReConApp2 and created project ReNotesBeta

---
+++ ReNotesBeta/ReNotesBeta/ReNotesBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReNotesBetaApp.swift
+//  ReNotesBeta
+//
+//  Created by Harry Dinh on 2022-06-25.
+//
+
+import SwiftUI
+
+@main
+struct ReNotesBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
