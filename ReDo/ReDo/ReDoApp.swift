c26e238e6721bd6ffcf15e414b7ae238e45d3a49
Harry Dinh <thongdinh2004@icloud.com>
2023-01-16 16:37:39 -0500

    Created project ReDo for Mac

---
+++ ReDo/ReDo/ReDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReDoApp.swift
+//  ReDo
+//
+//  Created by Harry Dinh on 2023-01-16.
+//
+
+import SwiftUI
+
+@main
+struct ReDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
