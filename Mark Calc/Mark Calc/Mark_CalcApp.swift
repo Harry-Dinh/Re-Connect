a2f463cbadd374b8a4d7b91117fee663a2d76ef2
Harry Dinh <thongdinh2004@icloud.com>
2023-02-23 16:51:33 -0500

    Created project Mark Calc

---
+++ Mark Calc/Mark Calc/Mark_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mark_CalcApp.swift
+//  Mark Calc
+//
+//  Created by Harry Dinh on 2023-02-23.
+//
+
+import SwiftUI
+
+@main
+struct Mark_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
