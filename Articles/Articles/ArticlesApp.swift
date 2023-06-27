dfe89e254c6cab07fede9baef1a7b510071d9de8
Harry Dinh <thongdinh2004@icloud.com>
2023-06-27 11:02:45 +0700

    Created project Articles for iPad

---
+++ Articles/Articles/ArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlesApp.swift
+//  Articles
+//
+//  Created by Harry Dinh on 2023-06-27.
+//
+
+import SwiftUI
+
+@main
+struct ArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
