f8defbc63710aacf7d3c4bbfebd65135587d965e
Harry Dinh <thongdinh2004@icloud.com>
2023-06-30 16:14:05 +0700

    Created project Re:Tasks

---
+++ ReTasks/ReTasks/ReTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReTasksApp.swift
+//  ReTasks
+//
+//  Created by Harry Dinh on 2023-06-30.
+//
+
+import SwiftUI
+
+@main
+struct ReTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
