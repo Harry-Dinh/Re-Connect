61b58dc51c7f16c98c40e3c9a0a025f56ce811e1
Harry Dinh <thongdinh2004@icloud.com>
2023-07-29 12:00:06 +0700

    Built UI for login view

--- Practice Projects/Checked/Checked/Views/LoginView.swift
+++ Practice Projects/Checked/Checked/Views/LoginView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // Header
+                HeaderView(title: "Checked", subtitle: "Get things done.", angle: 15, background: .pink)
+                
+                // Login form
+                Form {
+                    TextField("Email address", text: $email)
+                        .textFieldStyle(.plain)
+                    
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(.plain)
+                    
+                    Button {
+                        // Attempt login
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.blue)
+                            
+                            Text("Login")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                    }
+                    .padding()
+                }
+                .offset(y: -50)
+                
+                // Create account
+                
+                VStack {
+                    Text("New around here?")
+                    NavigationLink("Create an Account", destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+            }
+        }
     }
 }
 
