c7143573f8f8c007d27126fe22c5fb9dada04797
Harry Dinh <thongdinh2004@icloud.com>
2023-08-17 17:41:32 +0700

    Created manager classes and their methods

---
+++ ReChannels/ReChannels/Managers/DatabaseManager.swift
@@ -0,0 +1,13 @@
+//
+//  DatabaseManager.swift
+//  ReChannels
+//
+//  Created by Harry Dinh on 2023-08-17.
+//
+
+import Foundation
+import FirebaseDatabase
+
+class DatabaseManager: ObservableObject {
+    public static let shared = DatabaseManager()
+}
