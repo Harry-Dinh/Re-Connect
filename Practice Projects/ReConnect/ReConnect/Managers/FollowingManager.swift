ba5463a4b415b0e840cb445df40441c3f66b698c
Harry Dinh <thongdinh2004@icloud.com>
2023-12-17 11:34:41 -0500

    Re:Connect wrote some comments for the FollowingManager file

--- Practice Projects/ReConnect/ReConnect/Managers/FollowingManager.swift
+++ Practice Projects/ReConnect/ReConnect/Managers/FollowingManager.swift
@@ -10,6 +10,9 @@ import SwiftUI
 import FirebaseDatabase
 
 class FollowingManager: ObservableObject {
+    
+    // MARK: - REFERENCES TO OTHER FRAMEWORKS
+    
     public static let shared = FollowingManager()
     
     private let databaseRef = Database.database().reference()
@@ -20,39 +23,58 @@ class FollowingManager: ObservableObject {
     
     // MARK: - UPDATE FOLLOWING & FOLLOWER LISTS
     
+    /// Add a user to the following list of the *current* user, as well as updating that list data on the database.
+    /// - Parameter user: The user to add to the following list
     public func addUserToFollowingList(_ user: RECUser) {
+        // Add the following user to the local followings array
         loginVM.currentUser?.followings.append(user)
         
+        // Get a reference to the current user? Not sure why I did this before, maybe to prevent some sort of Swift's optional unwrapping issue?
         guard let currentUser = loginVM.currentUser else {
             return
         }
         
+        // Turn the passed in user data into JSON and update the followings array on the database
         let otherUserJSONData: [String: Any] = user.toDictionary()
         databaseRef.child(RECDatabaseParentPath.users).child(currentUser.firebaseUID).child(RECUser.Property.followings).updateChildValues(otherUserJSONData)
     }
     
+    
+    /// Add the *current* user to the *other* user's followers list. This function only gets called if the *other* user's account is not locked.
+    /// - Parameter user: The user to get a reference to the followers list to add the *current* user to.
     public func addCurrentUserToFollowerList(of user: RECUser) {
+        // Get a reference to the current user
         guard let currentUser = loginVM.currentUser else {
             return
         }
         
+        // Turn the user parameter into a variable (so it's mutable) then append that user's followers list
+        // Not sure if this step is necessary since you can reload the view afterward...
         var otherUser = user
         otherUser.followers.append(currentUser)
         
+        // Turn the current user's data into JSON and update the other user's database entry
         let currentUserJSONData: [String: Any] = currentUser.toDictionary()
         databaseRef.child(RECDatabaseParentPath.users).child(otherUser.firebaseUID).child(RECUser.Property.followers).updateChildValues(currentUserJSONData)
     }
     
+    
     // MARK: - FOLLOW & REQUEST FOLLOW ACTIONS
     
+    
+    /// A function that contains the actions that will occur when the *current* user taps the "Follow" button
+    /// - Parameter otherUser: The user to follow (this is passed in so that the app knows where to update the data on the receiving end)
     public func follow(_ otherUser: RECUser) {
+        // Get a reference to the current user
         guard let currentUser = loginVM.currentUser else {
             return
         }
         
+        // Call the functions to add the OTHER user to the CURRENT user's followings list; and the function that adds the CURRENT user to the OTHER user's followers list
         addUserToFollowingList(otherUser)
         addCurrentUserToFollowerList(of: otherUser)
         
+        // Create and send a notification to the OTHER user to inform them that they have a new follower
         let notification = RECNotification(title: "New Follower",
                                            notificationDescription: "\(currentUser.displayName) has started following you",
                                            iconURL: "",
@@ -62,11 +84,16 @@ class FollowingManager: ObservableObject {
         notificationManager.send(notification, to: otherUser)
     }
     
+    
+    /// Send a notification to the passed in user that have actions that allow them to accept/decline the following request
+    /// - Parameter user: The user to send the follow request notification to.
     public func requestToFollow(_ user: RECUser) {
+        // Get a reference to the current user
         guard let currentUser = loginVM.currentUser else {
             return
         }
         
+        // Create and send a notification to the passed in user
         let followRequestNotification = RECNotification(title: "New Follower Request",
                                                         notificationDescription: "\(currentUser.displayName) has requested to follow you",
                                                         iconURL: CUPSystemIcon.userRequestAction,
