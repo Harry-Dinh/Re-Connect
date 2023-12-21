5ce98be3c7492f0be0dc20467a10dcb5d79491f2
Harry Dinh <thongdinh2004@icloud.com>
2023-12-21 10:52:22 -0500

    C++ Practice--Added notes for C++ main method

--- C++ Practice/helloworld.cc
+++ C++ Practice/helloworld.cc
@@ -1,8 +1,9 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
+    // `cout` stands for "console out"
+    // `endl` stands for "end line"
     cout << "Hello world!" << endl;
     return 0;
 }
