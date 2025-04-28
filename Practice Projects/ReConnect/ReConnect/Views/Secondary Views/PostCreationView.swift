801c28e175e37ddf202c42ab881b41bc8cf8319f
Harry Dinh <harrydinh1207@gmail.com>
2025-04-28 13:58:13 -0400

    Added a file for the post creation screen

---
+++ Practice Projects/ReConnect/ReConnect/Views/Secondary Views/PostCreationView.swift
@@ -0,0 +1,20 @@
+//
+//  PostCreationView.swift
+//  ReConnect
+//
+//  Created by Harry Dinh on 2025-04-27.
+//
+
+import SwiftUI
+
+struct PostCreationView: View {
+    var body: some View {
+        Text("Bruhhhhhhh")
+    }
+}
+
+struct PostCreationView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostCreationView()
+    }
+}
